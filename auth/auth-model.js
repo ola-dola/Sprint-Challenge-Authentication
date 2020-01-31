9b62b288470f91c7431fe3e21f54087701eb8dcc
ola-dola <oredolaolamide@gmail.com>
2020-01-31 10:21:26 +0100

    added register endpoint

---
+++ auth/auth-model.js
@@ -0,0 +1,18 @@
+const db = require("../database/dbConfig");
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+module.exports = {
+  add,
+  findById
+};
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require("./auth-model");
+
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const hashPass = bcrypt.hashSync(password, 10);
+
+  const securedData = {
+    username,
+    password: hashPass
+  };
+
+  Users.add(securedData)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Unexpected server error" });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
